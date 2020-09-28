db9fb0af0056e8880accdb6ff2e78278b12069d2
jennyg0 <jen2348@hotmail.com>
2020-09-28 23:16:50 +0000

    added parser and random string generator

--- express_server.js
+++ express_server.js
@@ -9,6 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  newShortURL = Math.random().toString(36).substring(2,8);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +23,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
